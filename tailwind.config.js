bf1b4dc0ec101ba96539a9f483d2a1b4be1b50ec
Mustafa <mustafa.alroomi.91@gmail.com>
2024-10-01 21:10:02 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{tsx,css}"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      mono: [
+        "Monaco",
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Consolas",
+        "Liberation Mono",
+        "Courier New",
+        "monospace",
+      ],
+    },
+    container: {
+      center: "true",
+      screens: {
+        sm: "50rem",
+      },
+    },
+    extend: {
+      colors: {
+        slate: {
+          850: "hsl(222deg 47% 16%)",
+        },
+        primary: "#5fc3e7",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
